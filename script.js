23a23b6552602e0fc534ec8d8ca96b07d849d485
Rainnnn <93276447+FitriaFajarAria@users.noreply.github.com>
2022-10-09 13:57:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var calculate = document.querySelector(".calculate");
+
+calculate.addEventListener("click", findBMI);
+
+function findBMI() {
+  var height = +document.querySelector("#height").value;
+  var weight = +document.querySelector("#weight").value;
+
+  if (height <= 0 || weight <= 0 || isNaN(height) || isNaN(weight)) {
+    alert("Please fill all fields with valid numbers");
+    return;
+  }
+
+  height = height / 100;
+  var bmi = weight / (height * height);
+  showResult(bmi);
+}
+
+function showResult(bmi) {
+  var result = document.querySelector(".result");
+  var bmiStatus;
+  if (bmi < 18.5) {
+    bmiStatus = "Underweight";
+  } else if (bmi < 25) {
+    bmiStatus = "Normal";
+  } else if (bmi < 30) {
+    bmiStatus = "Overweight";
+  } else {
+    bmiStatus = "Obese";
+  }
+  result.innerHTML = `<div class=${bmiStatus}>Your BMI is ${bmi.toFixed(2)} and you are ${bmiStatus}.</div>`;
+}
